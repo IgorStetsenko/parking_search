95627c24e559fd7f2c4fcd3c7d819671b7d7c07b
Igor_Stetsenko <i.a.stetsenko@gmail.com>
2020-11-21 16:25:37 +0300

    Add class Video. Add main algoritm.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+class Video():
 
-while True:
-    ret, img = cap.read()
-    cv2.imshow("camera", img)
-    cv2.imwrite('screen.jpg', img)
-    if cv2.waitKey(10) == 27: # Клавиша Esc
-        break
-cap.release()
-cv2.destroyAllWindows()
+    def write_video(self):
+        '''Doc'''
+        cap = cv2.VideoCapture(0)
+            while True:
+                ret, img = cap.read()
+                cv2.imshow("camera", img)
+                cv2.imwrite('screen.jpg', img)
+                if cv2.waitKey(10) == 27: # Клавиша Esc
+                    break
+            cap.release()
+            cv2.destroyAllWindows()
+
+
+def main_algoritm():
+    pass
+
+if __name__ == "__main__":
+
+    main_algoritm()
