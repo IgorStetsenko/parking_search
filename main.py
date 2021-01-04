8c89343589f8ab2d4bad7b41f097d704942ddff5
IgorStetsenko <i.a.stetsenko@gmail.com>
2021-01-05 01:30:56 +0700

    Add class frame_utils

--- main.py
+++ main.py
@@ -50,13 +50,6 @@ class Camera_work():
         diff = cv2.absdiff(frame1, frame2)
         return diff
 
-
-
-
-
-
-
-
 class Data_calculate():
 
     def iou_calculate(self, box1, box2):
@@ -111,6 +104,26 @@ class Sms_delivery():
             to=""
         )
 
+class frame_utils():
+    def(self, contours):
+
+        # for contour in сontours:
+        #     (x, y, w, h) = cv2.boundingRect(
+        #         contour)  # преобразование массива из предыдущего этапа в кортеж из четырех координат
+        #
+        #     # метод contourArea() по заданным contour точкам, здесь кортежу, вычисляет площадь зафиксированного объекта в каждый момент времени, это можно проверить
+        #     # print(cv2.contourArea(contour))
+        #
+        #     if cv2.contourArea(contour) < 700:  # условие при котором площадь выделенного объекта меньше 700 px
+        #         continue
+        #     cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 255, 0), 2)  # получение прямоугольника из точек кортежа
+        #     cv2.putText(frame1, "Status: {}".format("dvigaetsya"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3,
+        #                 cv2.LINE_AA)  # вставляем текст
+        #
+        # cv2.drawContours(frame1, сontours, -1, (0, 0, 255), 2)  # также можно было просто нарисовать контур объекта
+
+
+
 
 # def main_algorithm(prediction_boxes):
 #     """
@@ -146,7 +159,6 @@ if __name__ == "__main__":
         cap.set(cv2.CAP_PROP_FPS, 25)
         ret, frame1 = cap.read()
         ret, frame2 = cap.read()
-        print(type(frame1))
         while cap.isOpened():  # метод isOpened() выводит статус видеопотока
             frame1 = cv2.rectangle(frame1, (1, 1), (960, 200), (0, 0, 0), -1)
             frame2 = cv2.rectangle(frame2, (1, 1), (960, 200), (0, 0, 0), -1)
@@ -161,17 +173,20 @@ if __name__ == "__main__":
             сontours, _ = cv2.findContours(dilated, cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_SIMPLE)  # нахождение массива контурных точек
             move_detector =  False if сontours == [] else True
-            # print(move_detector)
-            cv2.imshow("frame1", frame1)
+            print(move_detector)
+            # cv2.imshow("frame1", frame1)
             sleep(0.01)
             frame1 = frame2  #
             ret, frame2 = cap.read()  #
 
             if cv2.waitKey(40) == 27:
                 break
-            print(frame1)
             if move_detector:
-                detection_function(source_image)
+                cap.release()
+                cv2.destroyAllWindows()
+                prediction_boxes = detection_function(source_image)
+            else:
+                print("пауза")
 
         cap.release()
         cv2.destroyAllWindows()
